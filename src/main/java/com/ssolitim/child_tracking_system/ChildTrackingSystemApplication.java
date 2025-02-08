4bd765af88db3ee68dae6b373123c5c2918c8ae4
김원경 <kyg279044@gmail.com>
2025-02-08 13:21:42 +0900

    setting

---
+++ src/main/java/com/ssolitim/child_tracking_system/ChildTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ssolitim.child_tracking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssolitim/child_tracking_system/ChildTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssolitim.child_tracking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
